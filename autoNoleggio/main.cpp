4f84a3044a61471ebe307633f8c33d1239464c3f
Samuele Savino <riper.grimmer@gmail.com>
2023-01-23 22:24:21 +0100

    1 scrittura

---
+++ autoNoleggio/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+void visualizzazioneCatalogo(){
+
+}
+
+int main()
+{
+    int scelta;
+
+    while(true){
+        cout<<"     Menu       ";
+        cout<<"1- Inserire la macchina desiderata";
+        cout<<"2- Stampa del catalogo";
+        cout<<"3- Uscita";
+        cin>>scelta;
+        switch(scelta){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                return 0;
+                break;
+        }
+    }
+
+    return 0;
+}
