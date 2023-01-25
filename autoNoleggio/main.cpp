7be8b86ff85dfbb3378fca16dd17d421d4201676
Samuele Savino <riper.grimmer@gmail.com>
2023-01-25 22:19:44 +0100

    agg

--- autoNoleggio/main.cpp
+++ autoNoleggio/main.cpp
@@ -21,24 +21,44 @@ void visualizzazioneCatalogo(){
         getline(fin,temp);
         cout<<temp<<endl;
     }
+    fin.close();
+}
+
+void ricercaCampo(string cat,string giorni){
+    ifstream fin("auto.txt", ios::in);
+    int i=0;
+
+    while(!fin.eof()){
+        fin>>caratteristicheMacchina.Categoria;
+        fin>>caratteristicheMacchina.Marca;
+        fin>>caratteristicheMacchina.Modello;
+        fin>>caratteristicheMacchina.Colore;
+        fin>>caratteristicheMacchina.Settimana[i];
+        i++;
+    }
 }
 
 int main()
 {
     int scelta;
+    string categ,giorni;
 
     while(true){
         cout<<"     Menu       ";
-        cout<<"1- Inserire la macchina desiderata";
+        cout<<"1- Prenotazione di una macchina";
         cout<<"2- Stampa del catalogo";
         cout<<"3- Uscita";
         cin>>scelta;
         switch(scelta){
             case 1:
-
+                cout<<"Quale categoria di macchina si vuole prenotare ?";
+                cin>>categ;
+                cout<<"In quali giorni?";
+                cin>>giorni;
+                ricercaCampo(categ,giorni);
                 break;
             case 2:
-
+                visualizzazioneCatalogo();
                 break;
             case 3:
                 return 0;
